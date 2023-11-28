a9396a6ee907d8e9469037124187c9f723f1f25d
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-11-28 14:58:01 +0530

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -248,10 +248,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 caption=FIRST_PART_TEXT.format(escape_markdown(first_name)),
                 parse_mode=ParseMode.MARKDOWN,
             )
-            await asyncio.sleep(0.2)
-            guu = await update.effective_message.reply_text("🐾")
-            await asyncio.sleep(1.8)
-            await guu.delete()  # Await this line
             await update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(START_BTN),
